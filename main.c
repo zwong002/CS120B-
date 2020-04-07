f386448abf4d7217a6c4c3a7b0fcebe95e696175
Zachary Wong <zwong002@wch136-08.cs.ucr.edu>
2020-04-07 08:58:17 -0700

    Initializing repository with lab files

---
+++ main.c
@@ -0,0 +1,23 @@
+/*	Author: zwong002
+ *  Partner(s) Name: none
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRB = 0xFF;
+	PORTB = 0x00;
+	while(1){
+		PORTB = 0x0F;
+	}
+	return 1;
+}
